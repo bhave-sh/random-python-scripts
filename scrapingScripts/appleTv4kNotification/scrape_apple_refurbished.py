6c2ffcc8e51dcfc47371d6dc27414f8bdeb780d5
Bhave sh <48606642+bhave-sh@users.noreply.github.com>
2025-03-29 11:53:31 -0700

    N/A
    N/A

--- scrapingScripts/appleTv4kNotification/scrape_apple_refurbished.py
+++ scrapingScripts/appleTv4kNotification/scrape_apple_refurbished.py
@@ -28,8 +28,8 @@ driver.get(URL)
 element = driver.find_element_by_name("add-to-cart").get_attribute('class')
 driver.quit()
 
-my_token = "1205161681:AAHhCNw8IhHSVTFDZOIee0HXyhKuU-1w94U"
-chat_id = "713241830"
+my_token = ""
+chat_id = ""
 
 def send_telegram_message(msg, chat_id=chat_id, token=my_token):
     """
